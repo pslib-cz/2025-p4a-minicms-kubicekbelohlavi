de7f4b01636e5328a400d4cac5f24608d2678898
IvoBelohlav <ivo.belohlav.web@gmail.com>
2026-03-31 11:32:40 +0200

    fix postgres prisma adapter on vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,4 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient, type Prisma } from "@prisma/client";
 import { isSqliteDatabaseUrl, normalizeDatabaseUrl } from "@/lib/prisma-runtime";
@@ -18,7 +19,10 @@ function createPrismaClient() {
     });
   }
 
-  return new PrismaClient({ log });
+  return new PrismaClient({
+    adapter: new PrismaPg({ connectionString: databaseUrl }),
+    log,
+  });
 }
 
 export const prisma =
